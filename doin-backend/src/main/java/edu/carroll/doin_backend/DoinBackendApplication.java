d5ef2699d4daadb0d4426d9ae7a6224b869a8708
lpilp21 <lpilp21@freeuni.edu.ge>
2024-08-30 10:42:35 -0600

    added starter project

---
+++ doin-backend/src/main/java/edu/carroll/doin_backend/DoinBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.carroll.doin_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoinBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoinBackendApplication.class, args);
+	}
+
+}
---
+++ doin-backend/src/test/java/edu/carroll/doin_backend/DoinBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.carroll.doin_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoinBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
