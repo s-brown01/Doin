56eb42b84f8bd36bf48ab3e36d0c0cb344fe91d3
lpilp21 <lpilp21@freeuni.edu.ge>
2024-09-09 14:17:34 -0600

    added login/register pages

--- doin-frontend/src/app/app.component.ts
+++ doin-frontend/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from '@angular/router';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
 import { NotificationsBarComponent } from "./notifications-bar/notifications-bar.component";
 import { StoryBarComponent } from './story-bar/story-bar.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -14,4 +15,8 @@ import { StoryBarComponent } from './story-bar/story-bar.component';
 })
 export class AppComponent {
   title = 'doin-frontend';
+  constructor(private router: Router) {}
+  isBlankLayout(): boolean {
+    return this.router.url === '/login' || this.router.url === '/register' || this.router.url === '/forgot-password';
+  }
 }
--- doin-frontend/src/app/app.routes.ts
+++ doin-frontend/src/app/app.routes.ts
@@ -3,14 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DiscoverComponent } from './discover/discover.component';
 import { FriendsComponent } from './friends/friends.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'discover', component: DiscoverComponent },
   { path: 'friends', component: FriendsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/home' }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
