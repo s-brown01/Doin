baeb15f607def760df5d82ba2711d7391d0fb71f
lpilp21 <lpilp21@freeuni.edu.ge>
2024-09-19 15:49:41 -0600

    added friendships model

---
+++ doin-backend/src/main/java/edu/carroll/doin_backend/web/model/Friendship.java
@@ -0,0 +1,32 @@
+package edu.carroll.doin_backend.web.model;
+
+import edu.carroll.doin_backend.web.enums.FriendshipStatus;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "friendships")
+public class Friendship {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "friend_id")
+    private User friend;
+
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private FriendshipStatus status;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "confirmed_at")
+    private LocalDateTime confirmedAt;
+}
