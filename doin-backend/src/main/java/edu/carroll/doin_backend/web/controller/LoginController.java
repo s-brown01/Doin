63bddb11f49ec96f87cb3d10af93b128fa651931
s-brown01 <sbrown01@carroll.edu>
2024-10-02 13:06:43 -0600

    Making a token DTO'

--- doin-backend/src/main/java/edu/carroll/doin_backend/web/controller/LoginController.java
+++ doin-backend/src/main/java/edu/carroll/doin_backend/web/controller/LoginController.java
@@ -2,31 +2,31 @@ package edu.carroll.doin_backend.web.controller;
 
 import edu.carroll.doin_backend.web.dto.LoginDTO;
 import edu.carroll.doin_backend.web.dto.RegisterDTO;
-import edu.carroll.doin_backend.web.security.JwtUtil;
 import edu.carroll.doin_backend.web.dto.TokenDTO;
 
+import edu.carroll.doin_backend.web.repository.LoginRepository;
+import edu.carroll.doin_backend.web.security.TokenService;
 import edu.carroll.doin_backend.web.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.Console;
 
 @RestController
 @RequestMapping("api")
 public class LoginController {
   private static final Logger log = LoggerFactory.getLogger(LoginController.class);
 
-  private final JwtUtil jwtUtil;
+  private final TokenService tokenService;
   private final UserService userService;
+    private final LoginRepository loginRepository;
 
-  public LoginController(JwtUtil jwtUtil, UserService userService) {
-    this.jwtUtil = jwtUtil;
+    public LoginController(TokenService tokenService, UserService userService, LoginRepository loginRepository) {
+    this.tokenService = tokenService;
     this.userService = userService;
-  }
+        this.loginRepository = loginRepository;
+    }
 
   /**
    * Handles POST requests to the /api/login endpoint.
@@ -42,7 +42,7 @@ public class LoginController {
     // if the user is validated by our loginService...
     if (isValidUser) {
       // generate new token and store it in DTO
-      final String token = jwtUtil.generateToken(login.getUsername());
+      final String token = tokenService.generateToken(login.getUsername());
       final TokenDTO tokenDTO = new TokenDTO(token);
       // Return the DTO
       return ResponseEntity.ok(tokenDTO);
@@ -56,7 +56,6 @@ public class LoginController {
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody RegisterDTO register) {
         log.info("LoginController: new user {} registering", register.getUsername());
-        System.out.print("\n\n\n\n\n" + register.getUsername());
 
         boolean registered = userService.createNewUser(register);
 
@@ -67,6 +66,25 @@ public class LoginController {
             // return that the username or password is invalid, no more specific than that to not reveal info
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
         }
+    }
+
+    @PostMapping("/validateToken")
+    public ResponseEntity<String> validateToken(@RequestBody TokenDTO tokenDTO) {
+      log.info("LoginController: validating token");
+      log.debug("Toke" );
+
+        if (tokenDTO.getToken() == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid token");
+        }
+
+      boolean valid = userService.validateToken(tokenDTO);
+
+      if (valid) {
+          return ResponseEntity.ok("{}");
+      } else {
+          return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid token");
+      }
+
     }
 
 }
