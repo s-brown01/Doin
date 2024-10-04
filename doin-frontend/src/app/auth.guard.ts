7ad80dec6b9fac948c77a3742f54155b33a29eae
s-brown01 <sbrown01@carroll.edu>
2024-10-04 00:25:35 -0600

    auto-generating an auth guard in angular

---
+++ doin-frontend/src/app/auth.guard.ts
@@ -0,0 +1,13 @@
+/**
+ * This is an auto-generated class from angular. Created using the command "ng generate guard auth".
+ */
+import { CanActivateFn } from '@angular/router';
+
+/**
+ * This is method determines weather the specific route can be activated/accessed by the user.
+ * @param route
+ * @param state
+ */
+export const authGuard: CanActivateFn = (route, state) => {
+  return true;
+};
--- doin-frontend/src/app/services/auth.service.ts
+++ doin-frontend/src/app/services/auth.service.ts
@@ -2,19 +2,23 @@ import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
 import { HttpHeaders } from '@angular/common/http';
 
+/**
+ * Framework of this authorization service was found from <a href = https://www.digitalocean.com/community/tutorials/angular-route-guards#using-the-canactivate-route-guard>here</a>.
+ * It was refined to match specific project with help from ChatGPT
+ */
 @Injectable({
   providedIn: 'root'
 })
-export class AuthorizationService {
+export class AuthService {
 
   constructor(private apiService: ApiService) {}
 
   validateToken(token: string) {
     const tokenDTO = { token: token };
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-    
+
     return this.apiService.post('validateToken', tokenDTO, headers);
   }
 
-  
+
 }
