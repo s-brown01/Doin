2207fc07790270de4534a233676bb739a993c084
lpilp21 <lpilp21@freeuni.edu.ge>
2024-10-06 15:32:40 -0600

    added global error handler in frontend

---
+++ doin-frontend/src/app/utils/GlobalErrorHandler.ts
@@ -0,0 +1,14 @@
+import { ErrorHandler, Injectable, Injector } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class GlobalErrorHandler implements ErrorHandler {
+  constructor(private injector: Injector) {}
+
+  handleError(error: any): void {
+    console.error('An error occurred:', error);
+
+    const router = this.injector.get(Router);
+    router.navigate(['/error']);
+  }
+}
