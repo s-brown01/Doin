34b1cc656699272096a969cd4d25c67412d829d8
lpilp21 <lpilp21@freeuni.edu.ge>
2024-10-06 15:30:33 -0600

    added logging to images service

--- doin-backend/src/main/java/edu/carroll/doin_backend/web/service/ImageServiceImpl.java
+++ doin-backend/src/main/java/edu/carroll/doin_backend/web/service/ImageServiceImpl.java
@@ -3,50 +3,37 @@ package edu.carroll.doin_backend.web.service;
 import edu.carroll.doin_backend.web.exception.ResourceNotFoundException;
 import edu.carroll.doin_backend.web.model.Image;
 import edu.carroll.doin_backend.web.repository.ImageRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
-/**
- * The {@code ImageServiceImpl} class is an implementation of the {@link ImageService} interface.
- * It provides methods for saving and retrieving image entities from the data store.
- */
 @Service
 public class ImageServiceImpl implements ImageService {
 
     private final ImageRepository imageRepository;
+    private static final Logger logger = LoggerFactory.getLogger(ImageServiceImpl.class);
 
-    /**
-     * Constructs an {@code ImageServiceImpl} instance with the specified image repository.
-     *
-     * @param imageRepository the {@link ImageRepository} used for image data access.
-     */
     public ImageServiceImpl(ImageRepository imageRepository) {
         this.imageRepository = imageRepository;
     }
 
-    /**
-     * Saves the specified image entity to the data store.
-     * The creation timestamp is set to the current time before saving.
-     *
-     * @param image the {@link Image} object to be saved.
-     */
     @Override
     public void save(Image image) {
         image.setCreatedAt(LocalDateTime.now());
+        logger.info("Saving new image with details: {}", image);
         imageRepository.save(image);
+        logger.info("Image saved successfully with ID: {}", image.getId());
     }
 
-    /**
-     * Retrieves an image entity by its unique identifier.
-     *
-     * @param id the unique identifier of the image to retrieve.
-     * @return the {@link Image} object representing the retrieved image.
-     * @throws ResourceNotFoundException if no image with the specified ID exists.
-     */
     @Override
     public Image get(Long id) {
+        logger.info("Retrieving image with ID: {}", id);
         return imageRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Image not found"));
+                .orElseThrow(() -> {
+                    logger.error("Image with ID {} not found", id);
+                    return new ResourceNotFoundException("Image not found");
+                });
     }
 }
