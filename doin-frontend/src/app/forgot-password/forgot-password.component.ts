f89a05e29aa4dfd620af77248d5552788a7d29d0
s-brown01 <sbrown01@carroll.edu>
2024-10-08 00:00:24 -0600

    Adding element to forgot password so user can see error messages (and css), along with null checks for ts properties onResetPassword

--- doin-frontend/src/app/forgot-password/forgot-password.component.ts
+++ doin-frontend/src/app/forgot-password/forgot-password.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+import {AuthService} from "../services/auth.service";
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,11 +8,28 @@ import { Component } from '@angular/core';
   styleUrl: './forgot-password.component.css'
 })
 export class ForgotPasswordComponent {
+  constructor(private router: Router, private authService: AuthService) { }
+
   resetData = {
-    email: '',
+    username: '',
     securityQuestion: '',
     securityAnswer: ''
   }
-  onResetPassword(){}
+  errorMessage: string | null = null;
+
+  // what happens when the user clicks submit on HTML...
+  onResetPassword(){
+    // checking no data is null
+    if (!this.resetData.username ||
+      !this.resetData.securityQuestion ||
+      !this.resetData.securityAnswer ) {
+      this.errorMessage = "Please enter all information";
+      return;
+    }
+
+    this.errorMessage = null;
+
+
+  }
 
 }
