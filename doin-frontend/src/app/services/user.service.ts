91306514079902bd9752bcddb8e049964c2b7952
lpilp21 <lpilp21@freeuni.edu.ge>
2024-10-16 10:55:37 -0600

    added frontend user service

---
+++ doin-frontend/src/app/services/user.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { catchError, Observable, tap, throwError } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { ApiService } from './api.service';
+import { UserDTO } from '../dtos/user.dto';
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private baseUrl = 'http://localhost:8080/api';
+  constructor(private http: HttpClient, private apiService : ApiService) { }
+
+  getUserById(id: number): Observable<UserDTO> {
+    let params: any = {};
+    params.id = id;
+    return this.http.get<UserDTO>(`${this.baseUrl}/users`, {params});  }
+
+    getUserByName(username: string): Observable<UserDTO> {
+        let params: any = {};
+        params.username = username;
+      
+        console.log('Request URL:', `${this.baseUrl}/users`);
+        console.log('Request Params:', params);
+      
+        return this.http.get<UserDTO>(`${this.baseUrl}/users`, { params }).pipe(
+          tap((response) => console.log('Response:', response)),
+          catchError((error) => {
+            console.error('Error fetching user:', error);
+            return throwError(error);
+          })
+        );
+    }
+}
