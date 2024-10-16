dce899013b5f2b138a5d0569d114cebc062503be
lpilp21 <lpilp21@freeuni.edu.ge>
2024-10-16 10:53:29 -0600

    added user page component declaration

--- doin-frontend/src/app/app.module.ts
+++ doin-frontend/src/app/app.module.ts
@@ -26,6 +26,7 @@ import { FriendsComponent } from './friends/friends.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AppRoutingModule } from './app.routes';
+import { UserPageComponent } from './user-page/user-page.component';
 
 @NgModule({
   declarations: [
@@ -45,7 +46,8 @@ import { AppRoutingModule } from './app.routes';
     FriendListComponent,
     AddPostPopupComponent,
     StoryPopupComponent,
-    StoryBarComponent
+    StoryBarComponent,
+    UserPageComponent
   ],
   imports: [
     BrowserModule,
