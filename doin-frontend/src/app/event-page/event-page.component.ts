ded39b5722817916532f41f9279268d44b75f022
lpilp21 <lpilp21@freeuni.edu.ge>
2024-10-16 10:10:31 -0600

    added event page component in frontend

--- doin-frontend/src/app/event-page/event-page.component.ts
+++ doin-frontend/src/app/event-page/event-page.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Optional } from '@angular/core';
+import { EventDTO } from '../dtos/event.dto';
+import { EventService } from '../services/event.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-event-page',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrl: './event-page.component.css'
 })
 export class EventPageComponent {
+  event: EventDTO | null = null; 
+  constructor(private eventService: EventService, private route: ActivatedRoute) {}
 
+  ngOnInit(): void {
+    const eventId = Number(this.route.snapshot.paramMap.get('id'));
+      if (eventId) {
+      this.eventService.getEvent(eventId).subscribe(
+        (data: EventDTO) => {
+          this.event = data;
+        },
+        (error) => {
+          console.error('Error fetching event:', error);
+        }
+      );
+    } else {
+      console.error('Invalid event ID');
+    }
+  }
 }
