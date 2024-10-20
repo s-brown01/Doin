b7b43dfbe9175ee307229163d87c04c18a313c63
s-brown01 <sbrown01@carroll.edu>
2024-10-20 15:33:41 -0600

    Adding @Service to FriendService (and impl)

--- doin-backend/src/main/java/edu/carroll/doin_backend/web/service/FriendService.java
+++ doin-backend/src/main/java/edu/carroll/doin_backend/web/service/FriendService.java
@@ -1,4 +1,7 @@
 package edu.carroll.doin_backend.web.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface FriendService {
 }
--- doin-backend/src/main/java/edu/carroll/doin_backend/web/service/FriendServiceImpl.java
+++ doin-backend/src/main/java/edu/carroll/doin_backend/web/service/FriendServiceImpl.java
@@ -2,7 +2,9 @@ package edu.carroll.doin_backend.web.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
+@Service
 public class FriendServiceImpl implements FriendService {
     /**
      * A Logger to just for this class
