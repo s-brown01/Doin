6354691cd56c7b569ce56c9a48894bc50c2dbab9
s-brown01 <sbrown01@carroll.edu>
2024-10-20 15:52:26 -0600

    Removing findByUserandStatus and findByFriendAndStatus AND changing return of findFriendsOfFriends to a Set (to ensure uniqueness)

--- doin-backend/src/main/java/edu/carroll/doin_backend/web/repository/FriendRepository.java
+++ doin-backend/src/main/java/edu/carroll/doin_backend/web/repository/FriendRepository.java
@@ -8,15 +8,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Set;
 
 public interface FriendRepository extends JpaRepository<Friendship, Integer> {
-    List<Friendship> findByUserAndStatus(User user, FriendshipStatus status);
-    List<Friendship> findByFriendAndStatus(User user, FriendshipStatus status);
-
     @Query("SELECT f2.friend FROM Friendship f1 " +
             "JOIN Friendship f2 ON f1.friend = f2.user " +
             "WHERE f1.user = :user " +
             "AND f2.friend != :user " +
             "AND f2.friend NOT IN (SELECT f3.friend FROM Friendship f3 WHERE f3.user = :user)")
-    List<User> findFriendsOfFriends(@Param("user") User user);
+    Set<User> findFriendsOfFriends(@Param("user") User user);
 }
