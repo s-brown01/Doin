ee23f266cd7120349052a66974509823668c7a0c
s-brown01 <sbrown01@carroll.edu>
2024-10-20 15:27:54 -0600

    Creating a Friendship service to handle all friends activities (such as finding friends of friends)

---
+++ doin-backend/src/main/java/edu/carroll/doin_backend/web/service/FriendServiceImpl.java
@@ -0,0 +1,11 @@
+package edu.carroll.doin_backend.web.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FriendServiceImpl implements FriendService {
+    /**
+     * A Logger to just for this class
+     */
+    private static final Logger log = LoggerFactory.getLogger(FriendServiceImpl.class);
+}
