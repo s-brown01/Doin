6b81265c5efcc9ab24de5dc55e8ae68a0dc82220
s-brown01 <sbrown01@carroll.edu>
2024-10-20 15:39:59 -0600

    Adding a friendsOfFriends method, using a direct SQL statement

--- doin-backend/src/main/java/edu/carroll/doin_backend/web/repository/FriendRepository.java
+++ doin-backend/src/main/java/edu/carroll/doin_backend/web/repository/FriendRepository.java
@@ -4,10 +4,19 @@ import edu.carroll.doin_backend.web.enums.FriendshipStatus;
 import edu.carroll.doin_backend.web.model.Friendship;
 import edu.carroll.doin_backend.web.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface FriendRepository extends JpaRepository<Friendship, Integer> {
     List<Friendship> findByUserAndStatus(User user, FriendshipStatus status);
     List<Friendship> findByFriendAndStatus(User user, FriendshipStatus status);
+
+    @Query("SELECT f2.friend FROM Friendship f1 " +
+            "JOIN Friendship f2 ON f1.friend = f2.user " +
+            "WHERE f1.user = :user " +
+            "AND f2.friend != :user " +
+            "AND f2.friend NOT IN (SELECT f3.friend FROM Friendship f3 WHERE f3.user = :user)")
+    List<User> findFriendsOfFriends(@Param("user") User user);
 }
