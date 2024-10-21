829808dcfc4c30b444c1457f13571adb610b12b1
s-brown01 <sbrown01@carroll.edu>
2024-10-20 21:34:55 -0600

    adding starting functionality to loading list of friends

--- doin-frontend/src/app/friends/friends.component.ts
+++ doin-frontend/src/app/friends/friends.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { FriendshipDto } from "../dtos/friendship.dto";
+import { FriendListComponent } from "../shared/friend-list/friend-list.component";
+import { FriendService} from "../services/friend.service";
 
 @Component({
   selector: 'app-friends',
   templateUrl: './friends.component.html',
-  styleUrl: './friends.component.css'
+  styleUrls: ['./friends.component.css']
 })
 export class FriendsComponent {
+  friendsList: FriendshipDto[] = [];
+
+  constructor(private friendService: FriendService) {
+  }
+
+  ngOnInit(): void {
+    this.getFriends();
+  }
+
+  getFriends(): void {
+    this.friendService.getFriends().subscribe((data: FriendshipDto[]) => {
+      this.friendsList = data;
+    });
+  }
+
+  // trackByFriendID(index: number, friend: any) {
+  //   return friend.id;
+  // }
 
 }
