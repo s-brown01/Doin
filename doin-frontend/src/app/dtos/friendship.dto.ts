30a06b32ae5f9a06256b5183251419b68fdc366b
s-brown01 <sbrown01@carroll.edu>
2024-10-21 10:53:52 -0600

    adding a FriendshipStatus Enum to each friendship and setting the default to Pending

--- doin-frontend/src/app/dtos/friendship.dto.ts
+++ doin-frontend/src/app/dtos/friendship.dto.ts
@@ -1,12 +1,20 @@
 import { UserDTO } from "./user.dto";
 import {ImageDTO} from "./image.dto";
 
+export enum FriendshipStatus {
+  PENDING = 'PENDING',
+  CONFIRMED = 'CONFIRMED',
+  BLOCKED = 'BLOCKED',
+  REMOVED = 'REMOVED',
+  NOTADDED = 'NOT ADDED'
+}
+
 export class FriendshipDto {
   username: string;
-  status: string;
+  status: FriendshipStatus;
   profilePic: ImageDTO;
 
-  constructor(username: string, status: string, profilePic: ImageDTO) {
+  constructor(username: string, status: FriendshipStatus = FriendshipStatus.PENDING, profilePic: ImageDTO) {
     this.username = username;
     this.status = status;
     this.profilePic = profilePic;
