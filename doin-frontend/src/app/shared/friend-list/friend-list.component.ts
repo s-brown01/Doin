3ab89ccdb6a3cce3d8cb01abc9375d26f045e6c2
s-brown01 <sbrown01@carroll.edu>
2024-10-20 21:36:07 -0600

    adding properties

--- doin-frontend/src/app/shared/friend-list/friend-list.component.ts
+++ doin-frontend/src/app/shared/friend-list/friend-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FriendshipDto} from "../../dtos/friendship.dto";
+import {ImageDTO} from "../../dtos/image.dto";
 
 @Component({
   selector: 'app-friend-list',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './friend-list.component.css'
 })
 export class FriendListComponent {
+  friends: FriendshipDto[] = [];
+  friendData = {
+    username: '',
+    profilePic: ImageDTO
+  }
+
+
+
+  // constructor(username: string, profilePic ImageDTO) {
+  //
+  // }
 
 }
