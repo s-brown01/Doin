866c1f28698faad6133317bd24d7ffb6f46a1d4c
s-brown01 <sbrown01@carroll.edu>
2024-10-20 21:32:59 -0600

    Adding properties to dto

--- doin-frontend/src/app/dtos/friendship.dto.ts
+++ doin-frontend/src/app/dtos/friendship.dto.ts
@@ -1,1 +1,15 @@
-import { UserDTO} from "../../dtos/user.dto";
+import { UserDTO } from "./user.dto";
+import {ImageDTO} from "./image.dto";
+
+export class FriendshipDto {
+  username: string;
+  status: string;
+  profilePic: ImageDTO;
+
+  constructor(username: string, status: string, profilePic: ImageDTO) {
+    this.username = username;
+    this.status = status;
+    this.profilePic = profilePic;
+  }
+}
+
