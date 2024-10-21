44ddf62ffa3df8494537bde00d7fc5896d3364d6
s-brown01 <sbrown01@carroll.edu>
2024-10-21 10:55:55 -0600

    adding a fof (Friends of Friends) list to ts and to html

--- doin-frontend/src/app/friends/friends.component.ts
+++ doin-frontend/src/app/friends/friends.component.ts
@@ -1,7 +1,6 @@
-import { Component } from '@angular/core';
-import { FriendshipDto } from "../dtos/friendship.dto";
-import { FriendListComponent } from "../shared/friend-list/friend-list.component";
-import { FriendService} from "../services/friend.service";
+import {Component} from '@angular/core';
+import {FriendshipDto, FriendshipStatus} from "../dtos/friendship.dto";
+import {FriendService} from "../services/friend.service";
 import {ImageDTO} from "../dtos/image.dto";
 
 @Component({
@@ -11,6 +10,7 @@ import {ImageDTO} from "../dtos/image.dto";
 })
 export class FriendsComponent {
   friendsList: FriendshipDto[] = [];
+  fofList: FriendshipDto[] = [];
 
   constructor(private friendService: FriendService) {
   }
@@ -20,18 +20,17 @@ export class FriendsComponent {
   }
 
   getFriends(): void {
-    // this.friendService.getFriends().subscribe((data: FriendshipDto[]) => {
-    //   this.friendsList = data;
-    // });
     this.friendsList = [
-      new FriendshipDto("friend1", "CONFIRMED", new ImageDTO(1, "friend1", "test")),
-      new FriendshipDto("friend2", "CONFIRMED", new ImageDTO(1, "friend2", "test")),
-      new FriendshipDto("friend3", "CONFIRMED", new ImageDTO(1, "friend3", "test"))
+      new FriendshipDto("friend1", FriendshipStatus.CONFIRMED, new ImageDTO(1, "friend1", "test")),
+      new FriendshipDto("friend2", FriendshipStatus.CONFIRMED, new ImageDTO(1, "friend2", "test")),
+      new FriendshipDto("friend3", FriendshipStatus.CONFIRMED, new ImageDTO(1, "friend3", "test"))
+    ]
+    this.fofList = [
+      new FriendshipDto("fof 1", FriendshipStatus.NOTADDED, new ImageDTO(1, "friend1", "test")),
+      new FriendshipDto("fof 2", FriendshipStatus.NOTADDED, new ImageDTO(1, "friend2", "test")),
+      new FriendshipDto("fof 3", FriendshipStatus.NOTADDED, new ImageDTO(1, "friend3", "test"))
     ]
   }
 
-  // trackByFriendID(index: number, friend: any) {
-  //   return friend.id;
-  // }
 
 }
