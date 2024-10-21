b4e85b9a3a5825d88fbc69e2f337dec33cafdeb2
s-brown01 <sbrown01@carroll.edu>
2024-10-21 10:33:29 -0600

    hard coding 3 friendshipDTOs

--- doin-frontend/src/app/friends/friends.component.ts
+++ doin-frontend/src/app/friends/friends.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FriendshipDto } from "../dtos/friendship.dto";
 import { FriendListComponent } from "../shared/friend-list/friend-list.component";
 import { FriendService} from "../services/friend.service";
+import {ImageDTO} from "../dtos/image.dto";
 
 @Component({
   selector: 'app-friends',
@@ -23,9 +24,9 @@ export class FriendsComponent {
     //   this.friendsList = data;
     // });
     this.friendsList = [
-      // new FriendshipDto("friend1", "CONFIRMED", {url: 'pic1.jpg'}),
-      // new FriendshipDto("friend2", "PENDING", {url: 'pic2.jpg'}),
-      // new FriendshipDto("friend3", "CONFIRMED", {url: 'pic3.jpg'})
+      new FriendshipDto("friend1", "CONFIRMED", new ImageDTO(1, "friend1", "test")),
+      new FriendshipDto("friend2", "CONFIRMED", new ImageDTO(1, "friend2", "test")),
+      new FriendshipDto("friend3", "CONFIRMED", new ImageDTO(1, "friend3", "test"))
     ]
   }
 
