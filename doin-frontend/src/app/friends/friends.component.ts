e9489b0d4832bdb5da2d174020c73dbfe75a750e
s-brown01 <sbrown01@carroll.edu>
2024-10-21 20:24:30 -0600

    Adding possible error messages and find friends method

--- doin-frontend/src/app/friends/friends.component.ts
+++ doin-frontend/src/app/friends/friends.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {FriendshipDto, FriendshipStatus} from "../dtos/friendship.dto";
 import {FriendService} from "../services/friend.service";
 import {ImageDTO} from "../dtos/image.dto";
+import {error} from "@angular/compiler-cli/src/transformers/util";
 
 @Component({
   selector: 'app-friends',
@@ -9,9 +10,13 @@ import {ImageDTO} from "../dtos/image.dto";
   styleUrls: ['./friends.component.css']
 })
 export class FriendsComponent {
-  friendsList: FriendshipDto[] = [];
-  fofList: FriendshipDto[] = [];
+  searchFriends: FriendshipDto[] = [];
+  mayKnowList: FriendshipDto[] = [];
+
   fofErrorMessage: string | null = null;
+  searchErrorMessage: string | null = null;
+
+  searchInput: string | null = null;
 
   constructor(private friendService: FriendService) {
   }
@@ -22,7 +27,7 @@ export class FriendsComponent {
 
   getFriends(): void {
     this.friendService.getFriendsOfFriends().subscribe(data => {
-        this.fofList = data;
+        this.mayKnowList = data;
         this.fofErrorMessage = null;
       },
       error => {
@@ -31,13 +36,28 @@ export class FriendsComponent {
       })
   }
 
+  findFriend(){
+    if (!this.searchInput){
+      this.searchErrorMessage = "Please input something to search";
+      return;
+    }
+    this.friendService.getFriendByUsername(this.searchInput).subscribe(data => {
+      this.searchFriends = data;
+      this.searchErrorMessage = null
+    },
+      error => {
+      console.error("Error in searching: " + error);
+      this.searchErrorMessage = error.message;
+    })
+  }
+
   loadFriends(): void {
-    this.friendsList = [
+    this.searchFriends = [
       new FriendshipDto("friend1", FriendshipStatus.CONFIRMED, new ImageDTO(1, "friend1", "test")),
       new FriendshipDto("friend2", FriendshipStatus.CONFIRMED, new ImageDTO(1, "friend2", "test")),
       new FriendshipDto("friend3", FriendshipStatus.CONFIRMED, new ImageDTO(1, "friend3", "test"))
     ]
-    this.fofList = [
+    this.mayKnowList = [
       new FriendshipDto("fof 1", FriendshipStatus.NOTADDED, new ImageDTO(1, "friend1", "test")),
       new FriendshipDto("fof 2", FriendshipStatus.NOTADDED, new ImageDTO(1, "friend2", "test")),
       new FriendshipDto("fof 3", FriendshipStatus.NOTADDED, new ImageDTO(1, "friend3", "test"))
