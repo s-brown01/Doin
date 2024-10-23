9e3f9f5a9d7316a70455fe8a6f35fdbb3bfa6297
s-brown01 <sbrown01@carroll.edu>
2024-10-22 21:02:17 -0600

    Adding a hard coded friends list

--- doin-frontend/src/app/notifications-bar/notifications-bar.component.ts
+++ doin-frontend/src/app/notifications-bar/notifications-bar.component.ts
@@ -4,6 +4,8 @@ import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
 import { UserDTO } from '../dtos/user.dto';
 import { Observable } from 'rxjs';
+import {FriendshipDto, FriendshipStatus} from "../dtos/friendship.dto";
+import {ImageDTO} from "../dtos/image.dto";
 
 @Component({
   selector: 'app-notifications-bar',
@@ -12,6 +14,15 @@ import { Observable } from 'rxjs';
 })
 export class NotificationsBarComponent {
   user: UserDTO | null = null;
+
+  friendsList = [
+    new FriendshipDto("fof 1", FriendshipStatus.NOTADDED, new ImageDTO(1, "friend1", "test"), 1),
+    new FriendshipDto("fof 2", FriendshipStatus.NOTADDED, new ImageDTO(1, "friend2", "test"), 1),
+    new FriendshipDto("fof 3", FriendshipStatus.NOTADDED, new ImageDTO(1, "friend3", "test"), 1),
+    new FriendshipDto("fof 4", FriendshipStatus.NOTADDED, new ImageDTO(1, "friend4", "test"), 1),
+    new FriendshipDto("fof 5", FriendshipStatus.NOTADDED, new ImageDTO(1, "friend5", "test"), 1),
+    new FriendshipDto("fof 6", FriendshipStatus.NOTADDED, new ImageDTO(1, "friend6", "test"), 1)
+  ];
 
   constructor(private authService: AuthService, private router: Router) {}
 
