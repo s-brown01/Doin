b88e72013ce64ae491346dcc3c451f23bb6b3737
s-brown01 <sbrown01@carroll.edu>
2024-10-22 21:00:45 -0600

    adding an ID to the DTO

--- doin-frontend/src/app/dtos/friendship.dto.ts
+++ doin-frontend/src/app/dtos/friendship.dto.ts
@@ -1,23 +1,28 @@
 import { UserDTO } from "./user.dto";
-import {ImageDTO} from "./image.dto";
+import { ImageDTO } from "./image.dto";
 
 export enum FriendshipStatus {
   PENDING = 'PENDING',
   CONFIRMED = 'CONFIRMED',
   BLOCKED = 'BLOCKED',
   REMOVED = 'REMOVED',
-  NOTADDED = 'NOT ADDED'
+  NOTADDED = 'NOT-ADDED'
 }
 
 export class FriendshipDto {
   username: string;
   status: FriendshipStatus;
   profilePic: ImageDTO;
+  id: number;
 
-  constructor(username: string, status: FriendshipStatus = FriendshipStatus.PENDING, profilePic: ImageDTO) {
+  constructor(username: string,
+              status: FriendshipStatus = FriendshipStatus.PENDING,
+              profilePic: ImageDTO,
+              id: number) {
     this.username = username;
     this.status = status;
     this.profilePic = profilePic;
+    this.id = id;
   }
 }
 
