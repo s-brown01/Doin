d4b4c52550f314084f6834124fccdd3643eec26f
s-brown01 <sbrown01@carroll.edu>
2024-10-22 22:21:23 -0600

    commenting out all of the insert statements (chatGPT said this is causing the errors

--- doin-backend/src/main/resources/data.sql
+++ doin-backend/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO security_questions(id, question) VALUES (1, 'pet');
-INSERT INTO security_questions(id, question) VALUES (2, 'school');
-INSERT INTO security_questions(id, question) VALUES (3, 'city');
+# INSERT INTO security_questions(id, question) VALUES (1, 'pet');
+# INSERT INTO security_questions(id, question) VALUES (2, 'school');
+# INSERT INTO security_questions(id, question) VALUES (3, 'city');
--- doin-backend/src/test/resources/data.sql
+++ doin-backend/src/test/resources/data.sql
@@ -1,8 +1,3 @@
-# CREATE TABLE IF NOT EXISTS security_questions (
-#   id INT AUTO_INCREMENT PRIMARY KEY,
-#   question VARCHAR(255) NOT NULL
-# );
-
-INSERT INTO security_questions(id, question) VALUES (1, 'pet');
-INSERT INTO security_questions(id, question) VALUES (2, 'school');
-INSERT INTO security_questions(id, question) VALUES (3, 'city');
+# INSERT INTO security_questions(id, question) VALUES (1, 'pet');
+# INSERT INTO security_questions(id, question) VALUES (2, 'school');
+# INSERT INTO security_questions(id, question) VALUES (3, 'city');
