10a2bac04af2ca13c48471a3458f8ca1d1a3053d
s-brown01 <sbrown01@carroll.edu>
2024-10-24 00:43:01 -0600

    Adding methods to interface

--- doin-backend/src/main/java/edu/carroll/doin_backend/web/service/FriendService.java
+++ doin-backend/src/main/java/edu/carroll/doin_backend/web/service/FriendService.java
@@ -1,7 +1,14 @@
 package edu.carroll.doin_backend.web.service;
 
+import edu.carroll.doin_backend.web.dto.FriendshipDTO;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service
 public interface FriendService {
+    public Set<FriendshipDTO> getFriendsOfFriends(String username);
+    public boolean addFriend(String username1, String username2);
+    public boolean removeFriend(String username1, String username2);
+    public FriendshipDTO getFriend(String username);
 }
