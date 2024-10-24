a0be4a678419cdab204a35a20f880b2d67576ada
s-brown01 <sbrown01@carroll.edu>
2024-10-24 00:02:52 -0600

    FriendshipDTOs will only contain 1 Friendship, not a list of Friends

---
+++ doin-backend/src/main/java/edu/carroll/doin_backend/web/dto/FriendshipDTO.java
@@ -0,0 +1,10 @@
+package edu.carroll.doin_backend.web.dto;
+
+import edu.carroll.doin_backend.web.enums.FriendshipStatus;
+
+public class FriendshipDTO {
+    private int id;
+    private String username;
+    private FriendshipStatus status;
+    private String profilePic;
+}
