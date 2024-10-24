5e49426c97e434c8559236eeab1f51f07d72c150
s-brown01 <sbrown01@carroll.edu>
2024-10-24 00:37:42 -0600

    No one is using UserRepository, so removing

--- doin-backend/src/main/java/edu/carroll/doin_backend/web/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.carroll.doin_backend.web.repository;
-
-import edu.carroll.doin_backend.web.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-//    User findByUsername(String userName);
-}
