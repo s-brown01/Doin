b52f034fdfa7aff75bce6496b38582a185e84e1a
s-brown01 <sbrown01@carroll.edu>
2024-10-24 00:41:32 -0600

    Adding properties and getters/setters

--- doin-backend/src/main/java/edu/carroll/doin_backend/web/dto/FriendshipDTO.java
+++ doin-backend/src/main/java/edu/carroll/doin_backend/web/dto/FriendshipDTO.java
@@ -1,10 +1,70 @@
 package edu.carroll.doin_backend.web.dto;
 
 import edu.carroll.doin_backend.web.enums.FriendshipStatus;
+import edu.carroll.doin_backend.web.model.Image;
 
 public class FriendshipDTO {
     private int id;
     private String username;
     private FriendshipStatus status;
-    private String profilePic;
+    private Image profilePic;
+
+    public FriendshipDTO(int id, String username, FriendshipStatus status, Image profilePic) {
+        this.id = id;
+        this.username = username;
+        this.status = status;
+        this.profilePic = profilePic;
+    }
+
+    public FriendshipDTO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public FriendshipStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(FriendshipStatus status) {
+        this.status = status;
+    }
+
+    public Image getProfilePic() {
+        return profilePic;
+    }
+
+    public void setProfilePic(Image profilePic) {
+        this.profilePic = profilePic;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        FriendshipDTO that = (FriendshipDTO) o;
+        return id == that.id &&
+                username.equals(that.getUsername()) &&
+                status.equals(that.getStatus());
+    }
+
+
+
 }
