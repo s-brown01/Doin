2b26c14201d2ebd0d218230a0097a1ad68311ec1
s-brown01 <sbrown01@carroll.edu>
2024-10-24 16:37:27 -0600

    Changing parameter names to match the interface and adding a check to getFriendsOfFriends to make sure that the Set is at least 5 long, if not, it gets random users (to make it 5 long)

--- doin-backend/src/main/java/edu/carroll/doin_backend/web/service/FriendServiceImpl.java
+++ doin-backend/src/main/java/edu/carroll/doin_backend/web/service/FriendServiceImpl.java
@@ -39,51 +39,60 @@ public class FriendServiceImpl implements FriendService {
      * Retrieves the set of "friends of friends" for the given user.
      * Ensures that no null values are returned and logs the process.
      *
-     * @param username the username of the person to get mutual friends for.
+     * @param userUsername the username of the person to get mutual friends for.
      * @return A set of users who are friends of the user's friends, or an empty set if none found.
      */
     @Override
-    public FriendshipDTO[] getFriendsOfFriends(String username) {
-        log.trace("getFriendsOfFriends: getting the friends of friends for username {}", username);
-        List<User> foundUsers = loginRepo.findByUsernameIgnoreCase(username);
+    public FriendshipDTO[] getFriendsOfFriends(String userUsername) {
+        log.trace("getFriendsOfFriends: getting the friends of friends for username {}", userUsername);
+        List<User> foundUsers = loginRepo.findByUsernameIgnoreCase(userUsername);
         // if the username wasn't found, return an empty list
         if (foundUsers == null || foundUsers.isEmpty()) {
-            log.warn("getFriendsOfFriends: no users found for username {}", username);
+            log.warn("getFriendsOfFriends: no users found for username {}", userUsername);
             return new FriendshipDTO[0];
         }
         // if more than 1 user was found, return an empty list
         if (foundUsers.size() > 1) {
-            log.warn("getFriendsOfFriends: more than one user found for username {}", username);
+            log.warn("getFriendsOfFriends: more than one user found for username {}", userUsername);
             return new FriendshipDTO[0];
         }
+
         // get the user that was found
         User initialUser = foundUsers.get(0);
         // get the friends of friends
-        log.trace("getFriendsOfFriends: getting friends from friendsRepository for username {}", username);
+        log.trace("getFriendsOfFriends: getting friends from friendsRepository for username {}", userUsername);
         try {
             Set<FriendshipDTO> friends = friendRepo.findFriendsOfFriends(initialUser);
-            log.trace("getFriendsOfFriends: found {} friends for username {}", friends.size(), username);
-            log.trace("FriendController: converting set to a FriendshipDTO[] and returning it for user: {}", username);
+            log.trace("getFriendsOfFriends: found {} friends for username {}", friends.size(), userUsername);
+            // if less than 5 friends of friends were found, populate the rest with random users
+            // > 5 is ok, but make sure to have some suggestions
+            if (friends.size() < 5) {
+                log.trace("getFriendsOfFriends: no friends-of-friends found for username {}, fetching random users", userUsername);
+                Set<FriendshipDTO> randomUsers = friendRepo.findRandomUsersExcept(initialUser, 5 - friends.size());
+                // adding randomUsers to the friends Set
+                friends.addAll(randomUsers);
+            }
+            log.trace("FriendController: converting set to a FriendshipDTO[] and returning it for user: {}", userUsername);
             return friends.toArray(new FriendshipDTO[0]);
         } catch (Exception e) {
             // if there are any Exceptions, return an empty Set
-            log.warn("getFriendsOfFriends: error while getting friends of friends for username {}", username);
+            log.warn("getFriendsOfFriends: error while getting friends of friends for username {}", userUsername);
             return new FriendshipDTO[0];
         }
     }
 
     @Override
-    public boolean addFriend(String username1, String username2) {
+    public boolean addFriend(String userUsername, String friendUsername) {
         return false;
     }
 
     @Override
-    public boolean removeFriend(String username1, String username2) {
+    public boolean removeFriend(String userUsername, String friendUsername) {
         return false;
     }
 
     @Override
-    public FriendshipDTO getFriend(String username) {
+    public FriendshipDTO getFriend(String friendUsername) {
         return null;
     }
 }
