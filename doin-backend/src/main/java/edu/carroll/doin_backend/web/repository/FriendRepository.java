9f74ca0cd526895e4320596aa788f631bf74dc7a
s-brown01 <sbrown01@carroll.edu>
2024-10-24 15:17:28 -0600

    Changing return type to a Set of FrienshipDTOs instead of Friendships

--- doin-backend/src/main/java/edu/carroll/doin_backend/web/repository/FriendRepository.java
+++ doin-backend/src/main/java/edu/carroll/doin_backend/web/repository/FriendRepository.java
@@ -1,5 +1,6 @@
 package edu.carroll.doin_backend.web.repository;
 
+import edu.carroll.doin_backend.web.dto.FriendshipDTO;
 import edu.carroll.doin_backend.web.model.Friendship;
 import edu.carroll.doin_backend.web.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,20 +16,17 @@ import java.util.Set;
 public interface FriendRepository extends JpaRepository<Friendship, Integer> {
 
     /**
-     * Retrieves a set of users who are friends of the user's friends but are not directly friends
+     * Retrieves a set of FriendshipDTOs representing friends of the user's friends but are not directly friends
      * with the given user.
-     * <p>
-     * This query finds users who are connected to the given user through mutual friendships.
-     * <BR>
-     * This javadoc was created with help from chatGPT.
      *
      * @param user The {@link User} for whom to find friends of friends.
-     * @return A set of users who are friends of the user's friends but not direct friends of the user.
+     * @return A set of FriendshipDTOs who are friends of the user's friends but not direct friends of the user.
      */
-    @Query("SELECT f2.friend FROM Friendship f1 " +
+    @Query("SELECT new edu.carroll.doin_backend.web.dto.FriendshipDTO(f2.friend.id, f2.friend.username, f2.status, f2.friend.profilePicture) " +
+            "FROM Friendship f1 " +
             "JOIN Friendship f2 ON f1.friend = f2.user " +
             "WHERE f1.user = :user " +
             "AND f2.friend != :user " +
             "AND f2.friend NOT IN (SELECT f3.friend FROM Friendship f3 WHERE f3.user = :user)")
-    Set<Friendship> findFriendsOfFriends(@Param("user") User user);
+    Set<FriendshipDTO> findFriendsOfFriends(@Param("user") User user);
 }
