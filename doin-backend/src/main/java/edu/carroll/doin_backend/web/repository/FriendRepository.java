017d3f6ec57c4b6e93da0791fa84dc6c71ff47cc
s-brown01 <sbrown01@carroll.edu>
2024-10-24 16:34:48 -0600

    Adding javadoc

--- doin-backend/src/main/java/edu/carroll/doin_backend/web/repository/FriendRepository.java
+++ doin-backend/src/main/java/edu/carroll/doin_backend/web/repository/FriendRepository.java
@@ -19,6 +19,7 @@ public interface FriendRepository extends JpaRepository<Friendship, Integer> {
      * Retrieves a set of FriendshipDTOs representing friends of the user's friends but are not directly friends
      * with the given user.
      *
+     * <p>This function the custom query was generated with help from StackOverflow</p>
      * @param user The {@link User} for whom to find friends of friends.
      * @return A set of FriendshipDTOs who are friends of the user's friends but not direct friends of the user.
      */
@@ -29,4 +30,24 @@ public interface FriendRepository extends JpaRepository<Friendship, Integer> {
             "AND f2.friend != :user " +
             "AND f2.friend NOT IN (SELECT f3.friend FROM Friendship f3 WHERE f3.user = :user)")
     Set<FriendshipDTO> findFriendsOfFriends(@Param("user") User user);
+
+    /**
+     * Retrieves a set of random users, excluding the given user and their direct friends.
+     * This method is useful for suggesting new users to connect with when no friends-of-friends are found.
+     *
+     * <p>This query returns random users who are not directly friends with the specified user and excludes the user
+     * themselves. The number of users retrieved can be limited by the 'limit' parameter.</p>
+     *
+     * <p>This function the custom query was generated with help from StackOverflow</p>
+     *
+     * @param currentUser The {@link User} to exclude from the results, along with their direct friends.
+     * @param limit The maximum number of random users to retrieve.
+     * @return A set of {@link FriendshipDTO} objects representing random users, excluding the current user and their friends.
+     */
+    @Query("SELECT new edu.carroll.doin_backend.web.dto.FriendshipDTO(u.id, u.username, null, u.profilePicture) " +
+            "FROM User u " +
+            "WHERE u != :currentUser " +
+            "AND u NOT IN (SELECT f.friend FROM Friendship f WHERE f.user = :currentUser) " +
+            "ORDER BY RAND()")
+    Set<FriendshipDTO> findRandomUsersExcept(@Param("currentUser") User currentUser, @Param("limit") int limit);
 }
