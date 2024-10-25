1005e77dabf867d4110882c5c0d792d710e04878
s-brown01 <sbrown01@carroll.edu>
2024-10-24 22:38:46 -0600

    Adding block and unblock user methods and adding headers to all posts

--- doin-frontend/src/app/services/friend.service.ts
+++ doin-frontend/src/app/services/friend.service.ts
@@ -13,10 +13,8 @@ export class FriendService {
   private baseUrl = 'http://localhost:8080/api';
   constructor(private http: HttpClient, private apiService : ApiService, private authService: AuthService) { }
 
-
   getFriendsOfFriends(): Observable<FriendshipDto[]> {
-    const username = this.authService.getCurrentUser().username;
-    const headers = new HttpHeaders().set('Username', username);
+    const headers = this.getHeaders();
     return this.http.get<FriendshipDto[]>(this.baseUrl + "/friends", { headers });
   }
 
@@ -25,12 +23,27 @@ export class FriendService {
   }
 
   addFriend(friend : FriendshipDto): Observable<any>{
-    const username = this.authService.getCurrentUser().username;
-    const headers = new HttpHeaders().set('Username', username);
+    const headers = this.getHeaders();
     return this.apiService.post("/friends/add-friend", friend, headers );
   }
 
   removeFriend(friend : FriendshipDto): Observable<any>{
-    return this.apiService.post("/friends/remove-friend", friend)
+    const headers = this.getHeaders();
+    return this.apiService.post("/friends/remove-friend", friend, headers);
+  }
+
+  blockUser(friend : FriendshipDto): Observable<any>{
+    const headers = this.getHeaders();
+    return this.apiService.post("/friends/block-friend", friend, headers);
+  }
+
+  unblockUser(friend : FriendshipDto): Observable<any>{
+    const headers = this.getHeaders();
+    return this.apiService.post("/friends/unblock-friend", friend, headers);
+  }
+
+  private getHeaders(): HttpHeaders{
+    const username = this.authService.getCurrentUser().username;
+    return new HttpHeaders().set('Username', username);
   }
 }
