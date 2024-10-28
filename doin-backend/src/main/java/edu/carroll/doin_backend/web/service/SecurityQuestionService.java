e5841a65b3f8637f90065869760be3a78b4e9b39
s-brown01 <sbrown01@carroll.edu>
2024-10-28 16:29:30 -0600

    Removing commented out code and @Service annotation

--- doin-backend/src/main/java/edu/carroll/doin_backend/web/service/SecurityQuestionService.java
+++ doin-backend/src/main/java/edu/carroll/doin_backend/web/service/SecurityQuestionService.java
@@ -1,12 +1,7 @@
 package edu.carroll.doin_backend.web.service;
 
-import edu.carroll.doin_backend.web.dto.ValidateResult;
 import edu.carroll.doin_backend.web.model.SecurityQuestion;
-import org.springframework.stereotype.Service;
 
 public interface SecurityQuestionService {
-//    Integer findIDBySecurityQuestionValue(String securityQuestionValue);
-//    SecurityQuestion findSecurityQuestionByID(Integer securityQuestionID);
     SecurityQuestion getSecurityQuestionByValue(String securityQuestionValue);
-//    ValidateResult validateAndGetSecurityQuestionByValue(String securityQuestionValue);
 }
