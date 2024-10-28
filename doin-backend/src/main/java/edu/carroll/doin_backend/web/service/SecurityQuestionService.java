3213c83fcfee93569f161252a7714b4708880a42
s-brown01 <sbrown01@carroll.edu>
2024-10-28 11:30:00 -0600

    Finalizing the methods in interface and implementation

--- doin-backend/src/main/java/edu/carroll/doin_backend/web/service/SecurityQuestionService.java
+++ doin-backend/src/main/java/edu/carroll/doin_backend/web/service/SecurityQuestionService.java
@@ -1,5 +1,6 @@
 package edu.carroll.doin_backend.web.service;
 
+import edu.carroll.doin_backend.web.dto.ValidateResult;
 import edu.carroll.doin_backend.web.model.SecurityQuestion;
 import org.springframework.stereotype.Service;
 
@@ -7,5 +8,5 @@ public interface SecurityQuestionService {
 //    Integer findIDBySecurityQuestionValue(String securityQuestionValue);
 //    SecurityQuestion findSecurityQuestionByID(Integer securityQuestionID);
     SecurityQuestion getSecurityQuestionByValue(String securityQuestionValue);
-
+//    ValidateResult validateAndGetSecurityQuestionByValue(String securityQuestionValue);
 }
--- doin-backend/src/main/java/edu/carroll/doin_backend/web/service/SecurityQuestionServiceImpl.java
+++ doin-backend/src/main/java/edu/carroll/doin_backend/web/service/SecurityQuestionServiceImpl.java
@@ -1,8 +1,15 @@
 package edu.carroll.doin_backend.web.service;
 
+import edu.carroll.doin_backend.web.dto.ValidateResult;
+import edu.carroll.doin_backend.web.model.SecurityQuestion;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SecurityQuestionServiceImpl implements SecurityQuestionService {
+
+    @Override
+    public SecurityQuestion getSecurityQuestionByValue(String securityQuestionValue) {
+        return null;
+    }
 
 }
