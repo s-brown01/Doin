77aca4ab387290e4e23695843685d3c4e32c16b7
s-brown01 <sbrown01@carroll.edu>
2024-10-28 14:17:39 -0600

    creating the getSecurityQuestionByValue method and adding the body, also adding logging

--- doin-backend/src/main/java/edu/carroll/doin_backend/web/service/SecurityQuestionServiceImpl.java
+++ doin-backend/src/main/java/edu/carroll/doin_backend/web/service/SecurityQuestionServiceImpl.java
@@ -2,6 +2,7 @@ package edu.carroll.doin_backend.web.service;
 
 import edu.carroll.doin_backend.web.model.SecurityQuestion;
 import edu.carroll.doin_backend.web.repository.SecurityQuestionRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -22,26 +23,60 @@ public class SecurityQuestionServiceImpl implements SecurityQuestionService {
 
     @Override
     public SecurityQuestion getSecurityQuestionByValue(String securityQuestionValue) {
-        return null;
+        log.trace("getSecurityQuestionByValue: getting question {}", securityQuestionValue);
+        final Integer id = securityQuestionRepo.findIdByQuestion(securityQuestionValue);
+        if (id == null) {
+            log.warn("getSecurityQuestionByValue: could not find question {}", securityQuestionValue);
+            return null;
+        }
+        log.trace("getSecurityQuestionByValue: found question {} with id {}", securityQuestionValue, id);
+        try {
+            log.trace("getSecurityQuestionByValue: getting security question {}", securityQuestionValue);
+            final SecurityQuestion securityQuestion = securityQuestionRepo.getReferenceById(id);
+            if (securityQuestion == null || securityQuestion.getQuestion() == null) {
+                log.warn("getSecurityQuestionByValue: could not find question {}", securityQuestionValue);
+                return null;
+            }
+            if (!securityQuestion.getQuestion().equals(securityQuestionValue)) {
+                log.warn("getSecurityQuestionByValue: question ({}) does not mach input value {}", securityQuestion.getQuestion(), securityQuestionValue);
+                return null;
+            }
+            log.trace("getSecurityQuestionByValue: found and validated question {}", securityQuestionValue);
+            return securityQuestion;
+        } catch (EntityNotFoundException e) {
+            log.error("getSecurityQuestionByValue: could not find the entity for security question {}", securityQuestionValue);
+            return null;
+        } catch (Exception e) {
+            log.error("getSecurityQuestionByValue: Error {} occurred while trying to get security question {}", e.getMessage(), securityQuestionValue);
+            return null;
+        }
     }
 
     public void checkAndFillRepo() {
+        log.trace("checkAndFillRepo: checking and populating repository");
         List<SecurityQuestion> securityQuestions = securityQuestionRepo.findAll();
+        log.trace("checkAndFillRepo: found {} security questions", securityQuestions.size());
         // create all the SQ's that will be used in the repo
         SecurityQuestion petSQ = new SecurityQuestion("pet");
         SecurityQuestion schoolSQ = new SecurityQuestion("school");
         SecurityQuestion citySQ = new SecurityQuestion("city");
 
+        log.trace("checkAndFillRepo: checking what security questions exist");
         // make sure all 3 SecurityQuestions exist in repo
         if (!securityQuestions.contains(petSQ)) {
+            log.trace("checkAndFillRepo: adding security question {}", petSQ);
             securityQuestionRepo.save(petSQ);
         }
         if (!securityQuestions.contains(schoolSQ)) {
+            log.trace("checkAndFillRepo: adding security question {}", schoolSQ);
             securityQuestionRepo.save(schoolSQ);
         }
         if (!securityQuestions.contains(citySQ)) {
+            log.trace("checkAndFillRepo: adding security question {}", citySQ);
             securityQuestionRepo.save(citySQ);
         }
+        log.trace("checkAndFillRepo: security questions repo filled with {} questions", securityQuestions.size());
+
     }
 
 }
