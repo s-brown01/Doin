454ae3cdb2a4af40045c5e98a6a29bece0397077
s-brown01 <sbrown01@carroll.edu>
2024-10-28 19:43:48 -0600

    Removing data.sql from test directory since we dont need it for tests

--- doin-backend/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-# INSERT INTO security_questions(id, question) VALUES (1, 'pet');
-# INSERT INTO security_questions(id, question) VALUES (2, 'school');
-# INSERT INTO security_questions(id, question) VALUES (3, 'city');
