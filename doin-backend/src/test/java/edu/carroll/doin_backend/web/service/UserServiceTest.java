2aaa49b2cf9269d58214730a78223d21e544c3d4
s-brown01 <sbrown01@carroll.edu>
2024-11-04 20:02:06 -0700

    Moving the validateCredentials and createNewUser tests to 3 different tests (success, invalid, null)

--- doin-backend/src/test/java/edu/carroll/doin_backend/web/service/UserServiceTest.java
+++ doin-backend/src/test/java/edu/carroll/doin_backend/web/service/UserServiceTest.java
@@ -2,79 +2,101 @@ package edu.carroll.doin_backend.web.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import edu.carroll.doin_backend.web.model.SecurityQuestion;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import edu.carroll.doin_backend.web.dto.RegisterDTO;
-import edu.carroll.doin_backend.web.repository.SecurityQuestionRepository;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 
 @SpringBootTest
 @Transactional
 public class UserServiceTest {
-    private static final String username = "testUser";
+    private static final String username1 = "testUser";
     private static final String password = "testPassword";
+    final String invalidUsername = username1 + "FAKE";
+    final String invalidPassword = password + "FAKE";
+    final String validSQValue = "pet";
+    final String invalidSQValue = validSQValue + "FAKE";
+    RegisterDTO user1Data = new RegisterDTO(username1, password, validSQValue, "answer");
 
     @Autowired
     private UserService userService;
 
-    @Test
-    public void validateCredentials(){
-        // Happy
-        // unsuccessful login - no one in database yet
-        assertFalse(userService.validateCredentials(username, password), "validateCredentials: No Users - verifying that no users still returns false");
+    @Autowired
+    private SecurityQuestionService sqService;
 
-        // creating a new user, assuming it works - tested in other method
-        RegisterDTO newUser = new RegisterDTO(username, password, "pet", "answer");
-        assertTrue(userService.createNewUser(newUser), "validateCredentials: creating new user");
-        assertTrue(userService.validateCredentials(username, password), "validateCredentials: Success - should be the same credentials and be validated");
-
-        // Crappy
-        final String invalidUsername = username + "FAKE";
-        final String invalidPassword = password + "FAKE";
-        final String upperCasePassword = password.toUpperCase();
-        final String lowerCasePassword = password.toLowerCase();
-
-        assertFalse(userService.validateCredentials(invalidUsername, password), "validateCredentials: InvalidUsername - should not be validated with incorrect username");
-        assertFalse(userService.validateCredentials(invalidUsername, invalidPassword), "validateCredentials: InvalidUsernameAndPassword - should not be validated with incorrect username and password");
-        assertFalse(userService.validateCredentials(username, invalidPassword), "validateCredentials: InvalidPassword - should not be validated with incorrect password");
-        assertFalse(userService.validateCredentials(username, upperCasePassword), "validateCredentials: InvalidPassword - should not be validated with upper case password");
-        assertFalse(userService.validateCredentials(username, lowerCasePassword), "validateCredentials: InvalidPassword - should not be validated with lower case password");
-
-        // Crazy
-
-        assertFalse(userService.validateCredentials(null, null), "validateCredentials: NullCredentials - should not validate null credentials");
-        assertFalse(userService.validateCredentials(null, password), "validateCredentials: NullPassword - should not validate null username");
-        assertFalse(userService.validateCredentials(username, null), "validateCredentials: NullUsername - should not validate null username");
+    @BeforeEach
+    public void setUp() {
+        sqService.addSecurityQuestion(validSQValue);
     }
 
     @Test
-    public void createNewUser(){
-        // Happy
-        final String validQuestion = "pet";
-        final String validAnswer = "answer";
-        // creating a new user with a valid questions and answers
-        final RegisterDTO newUser = new RegisterDTO(username, password, validQuestion, validAnswer);
-        assertTrue(userService.createNewUser(newUser), "createNewUser: creating new user");
-        // trying to create the same user twice
-        assertFalse(userService.createNewUser(newUser), "createNewUser: should not create newUser twice");
+    public void createNewUser_Success() {
+        assertTrue(userService.createNewUser(user1Data), "User1 should be created successfully");
+        assertFalse(userService.createNewUser(user1Data), "User1 should not be created twice");
+        assertTrue(userService.validateCredentials(username1, password), "User1 should be validated");
+    }
 
-        // Crappy
-        // a username for the new user, a security question not in the entity, and an empty string for the answer
-        final String newUsername = "new" + username;
-        final String badQuestion = "invalid security question";
-        final String emptyAnswer = "";
-        // creating a new user with a null security question
-        final RegisterDTO newBadUser = new RegisterDTO(newUsername, password, badQuestion, emptyAnswer);
-        assertFalse(userService.createNewUser(newBadUser), "createNewUser: should not create a user with invalid SecurityQuestion");
+    @Test
+    public void createNewUser_Invalid() {
+        final String shortPassword = "pass";
+        final RegisterDTO badPassData = new RegisterDTO(username1, shortPassword, validSQValue, "answer");
+        final RegisterDTO invalidSQ = new RegisterDTO(username1, password, invalidSQValue, "answer");
+        final RegisterDTO takenUsername = new RegisterDTO(username1, password, validSQValue, "answer");
 
-        // Crazy
-        // try to create a user with null data
-        final RegisterDTO newNullUser = new RegisterDTO(null, null, null, null);
-        assertFalse(userService.createNewUser(newNullUser), "createNewUser: should not create a user with null values");
+        assertTrue(userService.createNewUser(user1Data), "User1 should be created successfully");
+        assertFalse(userService.createNewUser(takenUsername), "Duplicate username should not be created");
+        assertFalse(userService.createNewUser(badPassData), "User with short password should not be created");
+        assertFalse(userService.createNewUser(invalidSQ), "User should not be created successfully with invalid SQ");
+    }
+
+    @Test
+    public void createNewUser_Null() {
+        final RegisterDTO emptySQ = new RegisterDTO(username1, password, "", "answer");
+        final RegisterDTO nullSQ = new RegisterDTO(username1, password, null, "answer");
+        final RegisterDTO emptyUsername = new RegisterDTO("", password, validSQValue, "answer");
+        final RegisterDTO nullUsername = new RegisterDTO(null, password, validSQValue, "answer");
+        final RegisterDTO emptyPassword = new RegisterDTO(username1, "", validSQValue, "answer");
+        final RegisterDTO nullPassword = new RegisterDTO(username1, null, validSQValue, "answer");
+        final RegisterDTO nullData = new RegisterDTO(null, null, null, null);
+
+
+        assertFalse(userService.createNewUser(emptySQ), "User should not be created successfully with empty SQ");
+        assertFalse(userService.createNewUser(nullSQ), "User should not be created successfully with null SQ");
+        assertFalse(userService.createNewUser(emptyUsername), "User should not be created successfully with empty username");
+        assertFalse(userService.createNewUser(nullUsername), "User should not be created successfully with null username");
+        assertFalse(userService.createNewUser(emptyPassword), "User should not be created successfully with empty password");
+        assertFalse(userService.createNewUser(nullPassword), "User should not be created successfully with null password");
+        assertFalse(userService.createNewUser(nullData), "User should not be created successfully with null data");
+    }
+
+    @Test
+    public void validateCredentials_Success() {
+        userService.createNewUser(user1Data);
+        assertTrue(userService.validateCredentials(username1, password), "Correct username and password should return true");
+    }
+
+    @Test
+    public void validateCredentials_Invalid() {
+        assertFalse(userService.validateCredentials(username1, password), "No users should return false");
+        userService.createNewUser(user1Data);
+        final String upperCasePassword = password.toUpperCase();
+        final String lowerCasePassword = password.toLowerCase();
+
+        assertFalse(userService.validateCredentials(username1, invalidPassword), "Incorrect password should return false");
+        assertFalse(userService.validateCredentials(invalidUsername, password), "Incorrect username should return false");
+        assertFalse(userService.validateCredentials(invalidUsername, invalidPassword), "Incorrect credentials should return false");
+        assertFalse(userService.validateCredentials(username1, upperCasePassword), "Correct upper-case password should return false");
+        assertFalse(userService.validateCredentials(username1, lowerCasePassword), "Correct lower-case password should return false");
+    }
+
+    @Test
+    public void validateCredentials_Null(){
+        userService.createNewUser(user1Data);
+        assertFalse(userService.validateCredentials(null, null), "'null' credentials should return false");
+        assertFalse(userService.validateCredentials(null, password), "'null' username should return false");
+        assertFalse(userService.validateCredentials(username1, null), "'null' password should return false");
     }
 
 }
