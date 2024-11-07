f9a848f3de4aa29b7f790f3d63a131f064930d12
lpilp21 <lpilp21@freeuni.edu.ge>
2024-11-06 19:19:59 -0700

    adde user evetns get method

--- doin-frontend/src/app/services/event.service.ts
+++ doin-frontend/src/app/services/event.service.ts
@@ -10,12 +10,30 @@ export class EventService {
 
   private baseUrl = 'http://localhost:8080/api';
   constructor(private http: HttpClient, private apiService : ApiService) { }
-
+  
   getEvents(page: number, size: number): Observable<EventDTO[]> {
     return this.http.get<{ content: EventDTO[] }>(`${this.baseUrl}/events?page=${page}&size=${size}`)
       .pipe(
         map((response: { content: any; }) => response.content)
       );
+  }
+
+  getUserEvents(page: number, size: number): Observable<EventDTO[]> {
+    return this.http.get<{ content: EventDTO[] }>(`${this.baseUrl}/events/users/?page=${page}&size=${size}`)
+      .pipe(
+        map((response: { content: any; }) => response.content)
+      );
+  }
+
+  getPublicEvents(page: number, size: number): Observable<EventDTO[]> {
+    return this.http.get<{ content: EventDTO[] }>(`${this.baseUrl}/events/public?page=${page}&size=${size}`)
+      .pipe(
+        map((response: { content: any; }) => response.content)
+      );
+  }
+
+  getUpcomingEvents(): Observable<EventDTO[]> {
+    return this.http.get<EventDTO[]>(`${this.baseUrl}/events/upcoming`);
   }
 
   getEvent(id: Number): Observable<EventDTO> {
@@ -28,5 +46,11 @@ export class EventService {
 
   joinEvent(eventId: number, userId: number): Observable<boolean>{
     return this.http.post<boolean>(`${this.baseUrl}/events/${eventId}/join?userId=${userId}`, {});
+  }
+
+  addImage(eventId: number, file: File): Observable<boolean>{
+    const formData = new FormData();
+    formData.append('file', file);
+    return this.http.post<boolean>(`${this.baseUrl}/events/${eventId}/images`, formData);
   }
 }
