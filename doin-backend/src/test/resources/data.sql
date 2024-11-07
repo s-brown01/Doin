fe42e3d7ccc6caaeeb2d9598a94406f61413f2ad
lpilp21 <lpilp21@freeuni.edu.ge>
2024-11-06 19:23:32 -0700

    lookup data insert for test

--- doin-backend/src/test/resources/data.sql
+++ doin-backend/src/test/resources/data.sql
@@ -1,6 +1,6 @@
--- INSERT INTO security_questions(question) VALUES ('pet');
--- INSERT INTO security_questions(question) VALUES ('school');
--- INSERT INTO security_questions(question) VALUES ('city');
+INSERT INTO security_questions(question) VALUES ('pet');
+INSERT INTO security_questions(question) VALUES ('school');
+INSERT INTO security_questions(question) VALUES ('city');
 
 INSERT INTO event_types(id, name) VALUES (1, 'lunch');
 INSERT INTO event_types(id, name) VALUES (2, 'party');
