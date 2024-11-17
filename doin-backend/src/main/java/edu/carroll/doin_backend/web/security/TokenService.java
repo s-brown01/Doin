c49d227f1baf7b323f455d9f517107fb704be8d7
lpilp21 <lpilp21@freeuni.edu.ge>
2024-11-17 13:44:22 -0700

    added missing javadoc

--- doin-backend/src/main/java/edu/carroll/doin_backend/web/security/TokenService.java
+++ doin-backend/src/main/java/edu/carroll/doin_backend/web/security/TokenService.java
@@ -4,15 +4,16 @@ package edu.carroll.doin_backend.web.security;
  * Service interface for handling token operations.
  * <p>
  * This interface defines methods for generating and validating tokens,
- * as well as extracting usernames from tokens.
+ * as well as extracting user information from tokens.
  * </p>
  */
 public interface TokenService {
 
     /**
-     * Generates a token for the given username.
+     * Generates a token for the given username and user ID.
      *
      * @param username the username for which to generate a token
+     * @param id       the user ID for which to generate a token
      * @return the generated token as a string
      */
     String generateToken(String username, Integer id);
@@ -33,5 +34,11 @@ public interface TokenService {
      */
     String getUsername(String token);
 
+    /**
+     * Extracts the user ID from the given token.
+     *
+     * @param token the token from which to extract the user ID
+     * @return the user ID associated with the token
+     */
     Integer getUserId(String token);
 }
